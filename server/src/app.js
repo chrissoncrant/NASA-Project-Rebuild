a8443e4be6674626e45b7372a6e0a790f1510695
Chris Soncrant <christopher.soncrant@gmail.com>
2022-03-21 15:38:44 -0700

    added planets model, updated README

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Yo yo yo');
-})
-
 module.exports = app;
---
+++ server/src/models/planets.model.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const { parse } = require('csv-parse');
+
+const planets = [];
+
+function isHabitablePlanet(planet) {
+    return planet.koi_disposition === 'CONFIRMED' && planet.koi_insol > 0.36 && planet.koi_insol < 1.11 && planet.koi_prad < 1.6
+}
+
+fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', planet => {
+        if (isHabitablePlanet(planet)) {
+            planets.push(planet);
+        }
+    })
+    .on('error', err => {
+        console.log('Error received', err);
+    })
+    .on('end', () => {
+        console.log(`${planets.length} habitable planets found!`);
+    })
