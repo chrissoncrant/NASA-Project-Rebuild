68fc8b74c16cf93e0f8cc4a8eb28ac6d06957518
Chris Soncrant <christopher.soncrant@gmail.com>
2022-03-22 11:37:42 -0700

    added functionality for planet data to load on server start

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -9,25 +9,37 @@ function isHabitablePlanet(planet) {
     return planet.koi_disposition === 'CONFIRMED' && planet.koi_insol > 0.36 && planet.koi_insol < 1.11 && planet.koi_prad < 1.6
 }
 
-fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
-    .pipe(parse({
-        comment: '#',
-        columns: true
-    }))
-    .on('data', planet => {
-        if (isHabitablePlanet(planet)) {
-            planets.push(planet);
-        }
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true
+            }))
+            .on('data', planet => {
+                if (isHabitablePlanet(planet)) {
+                    planets.push(planet);
+                }
+                
+            })
+            .on('error', err => {
+                console.log('Error received', err);
+                reject(err);
+            })
+            .on('end', () => {
+                console.log(`${planets.length} habitable planets found!`);
+                resolve();
+            })
     })
-    .on('error', err => {
-        console.log('Error received', err);
-    })
-    .on('end', () => {
-        console.log(`${planets.length} habitable planets found!`);
-    })
+}
+
+        
 
 function getHabitablePlanets() {
     return planets;
 }
 
-    module.exports = getHabitablePlanets;
+module.exports = {
+    loadPlanetsData,
+    getHabitablePlanets
+};
