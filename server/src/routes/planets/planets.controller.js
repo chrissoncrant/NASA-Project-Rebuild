3bf1cef92055f004bd73e4988d8c5b5f3092bc46
Chris Soncrant <christopher.soncrant@gmail.com>
2022-03-22 11:36:58 -0700

    added the controller and router for the planets model

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const getHabitablePlanets = require('../../models/planets.model')
+const { getHabitablePlanets } = require('../../models/planets.model')
 
 function httpGetAllPlanets(req, res) {
     res.status(200).json(getHabitablePlanets())
